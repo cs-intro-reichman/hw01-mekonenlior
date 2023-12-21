f0f42808f44e2022df52d18a2a52f28ae5bd2d47
Lior Mekonen <lior.mekonen01@post.runi.ac.il>
2023-12-21 21:49:38 +0200

    This is my 2nd commit

--- GenThree.java
+++ GenThree.java
@@ -2,8 +2,29 @@
  * Generates three random integers, each in a given range [a,b),
  * prints them, and then prints the minimal number that was generated.
  */
+
+import java.util.Random;
+
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+		int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+
+        if (num1 >= num2) {
+            System.out.println("Invalid range. Number 2 must be greater than number 1.");
+            return;
+        }
+
+        Random random = new Random();
+        int random1 = random.nextInt(num2 - num1) + num1;
+        int random2 = random.nextInt(num2 - num1) + num1;
+        int random3 = random.nextInt(num2 - num1) + num1;
+
+        System.out.println(random1);
+        System.out.println(random2);
+        System.out.println(random3);
+
+        int minNum = Math.min(Math.min(random1, random2), random3);
+        System.out.println("The minimal generated number was " + minNum);
 	}
 }
--- Triangle.java
+++ Triangle.java
@@ -5,6 +5,10 @@
  */ 
 public class Triangle {
 	public static void main(String[] args) {
-		// Put your code here	
+		int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+        boolean ValidTriangle = (a + b > c) && (a + c > b) && (b + c > a);
+        System.out.println("a: " + a + ", b: " + b + ", c: " + c + ", =: " + ValidTriangle);	
 	}
 }
